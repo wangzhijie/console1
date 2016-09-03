3dc3364b046dd6dc8466c8318629a4fa251f8331
wangzhijie <962402242@qq.com>
2016-09-03 17:45:46 +0800

    "为class1类添加除法功能"

--- ConsoleApplication1/Class1.cs
+++ ConsoleApplication1/Class1.cs
@@ -8,5 +8,24 @@ namespace ConsoleApplication1
 {
     class Class1
     {
+        private int x = 0;
+        private int y = 1;
+        public void count()
+        {
+            try
+            {
+                int m=x / y;
+                if(m>0){
+                    Console.WriteLine("m大于0");
+                }
+                else
+                {
+                    Console.WriteLine("m小于等于0");
+                }
+            }catch(Exception e){
+
+            }
+        }
+
     }
 }
